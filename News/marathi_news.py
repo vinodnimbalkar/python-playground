d500d69d4ad47a52ca0f3fc1ab387f3baf70086c
VinodNimbalkar <omvinod@yahoo.com>
2020-06-02 23:20:15 +0530

    bug fix

--- News/marathi_news.py
+++ News/marathi_news.py
@@ -1,13 +1,12 @@
-#!/usr/bin/python (No need to write Python before file)
-import urllib.request
+import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 
 #specify the url of marathi news website
-url = "http://www.lokmat.com/"
+url = "http://lokmat.com/"
 
-page = urllib.request.urlopen(url)
-soup = BeautifulSoup(page, 'html.parser')
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
 news = soup.find('ul', class_='live-news-list')
 
 for i in news.findAll('li'):
